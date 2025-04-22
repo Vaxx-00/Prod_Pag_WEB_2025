a3f7523f7a51d4fa0351697c2a26647d2f17d442
DarioIEL <dario.mennillo@immaginazioneelavoro.it>
2025-04-22 19:19:52 +0200

    u

--- Lez6-StatementFondamentali/js/script09.js
+++ Lez6-StatementFondamentali/js/script09.js
@@ -4,20 +4,33 @@
 
 //1 soluzione con for
 //1 soluzione con while
-//1 soluzione solo con for senza if
+//1 soluzione con solo 2 for e 1 if innestato
+//1 soluzione solo con for senza if ma con un array da 90 numeri (splice)
+
+// let numeriEstratti = [];
+// let tentativi = 0;
+
+// for(let i = 0 ; i < 20; i++){
+//     let numEstratto = Math.ceil(Math.random() * 90);
+//     if(numeriEstratti.includes(numEstratto)){
+//         i--; //fai un altro giro
+//     }else{
+//         numeriEstratti.push(numEstratto);
+//     }
+//     tentativi++;
+// }
+
+//console.log(numeriEstratti);
+//console.log(tentativi);
 
 let numeriEstratti = [];
 let tentativi = 0;
 
-for(let i = 0 ; i < 20; i++){
+while(numeriEstratti.length < 10){
     let numEstratto = Math.ceil(Math.random() * 90);
     if(numeriEstratti.includes(numEstratto)){
-        i--; //fai un altro giro
+       continue;
     }else{
         numeriEstratti.push(numEstratto);
     }
-    tentativi++;
 }
-
-console.log(numeriEstratti);
-console.log(tentativi);
